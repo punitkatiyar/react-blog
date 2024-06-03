bdd86aa878b4646aa5be15b6ceb9febf6ab802dc
Punit Katiyar <punit.kat@gmail.com>
2024-06-03 20:27:50 +0530

    Update App.js

--- blog-app/src/App.js
+++ blog-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Counter from  './components/Counter'
 function App() {
   return (
     <>
     <h1>Welcome to React App With Github</h1>
+    <Counter/>
     </>
   );
 }
